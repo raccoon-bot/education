ad006f991cb781b2cac1e3e327e02487848463cf
mhalych <Maksym.Halych@abraxas.ch>
2019-10-29 22:45:48 +0200

    Added calculation for simple numbers

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,14 @@ public class Main {
     public static void main(String ...args) {
         System.out.println("Hello World!");
         System.out.println("Just do it!");
+
+        int b = add(1L, 10.0f);
+
+        System.out.println(b);
+
+    }
+
+    public static <T extends Number> int add(T number1, T number2) {
+        return number1.intValue() + number2.intValue();
     }
 }
