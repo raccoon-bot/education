3c9a8f9757b0de1d2148e797ff5112060ea61022
mhalych <Maksym.Halych@abraxas.ch>
2019-10-29 22:50:00 +0200

    Added some shit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ public class Main {
     public static void main(String ...args) {
         System.out.println("Hello World!");
         System.out.println("Just do it!");
+
+        int a = 7;
+        int b = 12;
+
+        System.out.println("a" + a + b + "a" + b);
     }
 }
